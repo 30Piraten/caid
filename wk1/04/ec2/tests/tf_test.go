bc6c34b8340ed8196cca42be67f03bfceeb59f25
30Piraten <victorehikioyaraeva@outlook.com>
2025-03-23 23:09:12 +0100

    moved terraform code from root to config folder

--- wk1/04/ec2/tests/tf_test.go
+++ wk1/04/ec2/tests/tf_test.go
@@ -1,1 +1,25 @@
 package tests
+
+/*
+Defining test cases for infrastructure config on Terraform with Terratest.
+*/
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEC2Instance(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../config/instance.tf",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	instanceId := terraform.Output(t, terraformOptions, "ec2_instance_id")
+	assert.Equal(t, "ec2_instance_id", instanceId)
+}
--- wk1/04/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/30Piraten/resources/wk1/04
-
-go 1.23.4
-
-require github.com/aws/aws-lambda-go v1.47.0
