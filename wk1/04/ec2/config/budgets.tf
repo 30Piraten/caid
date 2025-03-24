c3e37ab62bbfdfbf6ee94cefdbdf0645d6e8edba
REV <victorehikioyaraeva@outlook.com>
2025-03-24 21:51:16 +0200

    Update budgets.tf

--- wk1/04/ec2/config/budgets.tf
+++ wk1/04/ec2/config/budgets.tf
@@ -1,3 +1,7 @@
+resource "aws_sns_topic" "budget_alerts" {
+  name = "budget-alerts"
+}
+
 # Cost monitoring with AWS Budgets
 resource "aws_budgets_budget" "ec2_budgets" {
   name = "ec2-monthly-budget"
@@ -16,5 +20,8 @@ resource "aws_budgets_budget" "ec2_budgets" {
     threshold = 80 
     threshold_type = "PERCENTAGE"
     notification_type = "ACTUAL"
+
+    subscriber_sns_topic_arns = [ aws_sns_topic.budget_alerts.arn ]
   }
+  
 }
